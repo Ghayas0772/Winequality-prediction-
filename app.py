57ef5786d1eeb2a76b81f689baf1e679cb66c4b2
Ghayasudin Ghayas <ghayasudin.edu@gmail.com>
2025-11-16 19:53:09 -0500

    Prepare app for Heroku deployment

--- app.py
+++ app.py
@@ -72,6 +72,6 @@ if __name__ == "__main__":
     import os
     # Heroku assigns a dynamic port, default to 5000 for local testing
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)
+    app.run(host="0.0.0.0", port=port, debug=True)
 
 
